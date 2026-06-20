f83938ee65839b1e94a4f1e7cad8ae136780e866
kanghyeon5468 <rkdgus5468@gmail.com>
2026-06-20 23:37:53 +0100

    Refactor sales simulator into feature module

---
+++ web/src/features/sales-simulator/index.ts
@@ -0,0 +1,7 @@
+export { default as SalesSimulator } from "./SalesSimulator";
+export {
+  calculateSalesSimulation,
+  DEFAULT_SALES_INPUTS,
+  type SalesInputs,
+  type SalesSimulation,
+} from "./model";
